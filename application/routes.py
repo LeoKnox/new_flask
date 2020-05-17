95a0b59ce21d07e617125ea9df1396a266a90c4e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-17 00:01:49 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -15,9 +15,16 @@ def dungeon():
     ]
     return render_template("dungeon.html", dungeon="active", info=info)
 
-@app.route("/room")
-def room():
+@app.route("/room/")
+@app.route("/room/<room_name>")
+def room(room_name="entry"):
+    info = [
+        {"room_name":"Entry","length":5,"width":5},
+        {"room_name":"Storage","length":9,"width":7},
+        {"room_name":"Insanity","length":8,"width":12}
+    ]
     room_data = {"room_name":"Entry", "length":5, "width":5}
+    room_data = next(item for item in info if item["room_name"] == room_name)
     return render_template("room.html", room="active", info=room_data)
 
 @app.route("/map")
